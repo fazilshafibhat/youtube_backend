235859722ba98a3ef5c38fd53f402f1f84d22f14
FAZIL SHAFI <fazilshafi.fsb382@gmail.com>
2024-04-27 11:36:02 +0530

    util code completion

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 // require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
-
-
+  path: "./env",
+});
 
 connectDB()
-
-
-
-
-
-
-
-
-
+  .then(() => {
+    app.on("errror", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 7000, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection FAILED !!! ", err);
+  });
 
 /*
 import express from "express"
